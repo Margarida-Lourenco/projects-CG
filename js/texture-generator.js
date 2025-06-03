c9c70da2407097f5311292e0352f4b69be08b9fe
Francisco Fernandes <francisco.r.fernandes@tecnico.ulisboa.pt>
2025-06-03 19:41:47 +0100

    fix texture generator

--- js/texture-generator.js
+++ js/texture-generator.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { createFloralFieldTexture, createStarrySkyTexture, createCheeseTexture } from './procedural-textures.js';
 
 let scene, camera, renderer;
-let textureCanvas;
+let textureCanvas, textureContext;
 let planeMesh, planeMaterial;
 
 const TEXTURE_WIDTH = 512;
